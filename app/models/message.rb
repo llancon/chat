8000ff3b03f0e4a2be29ef673f0721befbe2058e
llancon <rdzalb@gmail.com>
2023-05-17 16:40:09 -0400

    Turbo stream to rooms.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,8 @@
 class Message < ApplicationRecord
   belongs_to :room
+  broadcasts_to :room
+  # broadcasts_to replaces the below 3 model callbacks
+  # after_create_commit -> { broadcast_append_to room }
+  # after_destroy_commit -> { broadcast_remove_to room }
+  # after_update_commit -> { broadcast_replace_to room }
 end
